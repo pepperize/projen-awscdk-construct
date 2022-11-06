abce17bfadc095067387269cb79a011169eeb6a4
Patrick Florek <patrick.florek@gmail.com>
2022-11-06 16:46:59 +0100

    chore: configure subproject

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,83 @@
 import { TurborepoProject } from 'projen-turborepo';
+import { javascript, cdk } from "projen";
+
 const project = new TurborepoProject({
+  authorName: "Patrick Florek",
+  authorEmail: "patrick.florek@gmail.com",
+  authorOrganization: true,
+  license: "MIT",
+  copyrightOwner: "Pepperize UG (haftungsbeschränkt)",
+  name: "projen-components",
+  packageName: "@pepperize/projen-components",
   defaultReleaseBranch: 'main',
   devDeps: ['projen-turborepo'],
-  name: 'projen-awscdk-construct',
   projenrcTs: true,
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  pathMapping: true,
+  jestModuleNameMapper: true,
+  projectReferences: true,
+  parallelWorkflows: true,
+  vscodeMultiRootWorkspaces: true,
+
+  gitignore: [".idea/", "*.iml", ".vscode"],
 });
+
+new cdk.JsiiProject({
+  author: "Patrick Florek",
+  authorAddress: "patrick.florek@gmail.com",
+  authorOrganization: true,
+  license: "MIT",
+  copyrightOwner: "Pepperize UG (haftungsbeschränkt)",
+  name: "@pepperize/projen-awscdk-construct",
+  outdir: "packages/awscdk-construct",
+  description: "This project provides a projen project type providing presets for an AWS CDK construct library project",
+  keywords: ["aws", "cdk", "projen"],
+  repositoryUrl: "https://github.com/pepperize/projen-awscdk-construct.git",
+
+  projenrcTs: true,
+
+  autoApproveUpgrades: true,
+  autoApproveOptions: {
+    allowedUsernames: ["pflorek", "acfo", "dependabot[bot]"],
+    secret: "GITHUB_TOKEN",
+  },
+  dependabot: true,
+  dependabotOptions: {
+    labels: ["auto-approve"],
+  },
+
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      printWidth: 120,
+    },
+  },
+
+  defaultReleaseBranch: "main",
+  npmAccess: javascript.NpmAccess.PUBLIC,
+  release: true,
+  releaseToNpm: true,
+  // publishToNuget: {
+  //   dotNetNamespace: "Pepperize.Projen",
+  //   packageId: "Pepperize.Projen.AwscdkConstruct",
+  // },
+  publishToPypi: {
+    distName: "pepperize.projen-awscdk-construct",
+    module: "pepperize_projen_awscdk_construct",
+  },
+  publishToMaven: {
+    mavenEndpoint: "https://s01.oss.sonatype.org",
+    mavenGroupId: "com.pepperize",
+    mavenArtifactId: "projen-awscdk-construct",
+    javaPackage: "com.pepperize.projen.awscdk_construct",
+  },
+
+  deps: ["projen"],
+  peerDeps: ["projen"],
+
+  scripts: {
+    "format": "prettier --write src/**/*.ts test/**/*.ts .projenrc.[jt]s README.md"
+  },
+});
+
 project.synth();
