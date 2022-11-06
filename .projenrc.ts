89accba0627f87f640417ba5432eb7e785ca8b87
Patrick Florek <patrick.florek@gmail.com>
2022-11-06 16:10:34 +0100

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,12 @@
+import { TurborepoProject } from 'projen-turborepo';
+const project = new TurborepoProject({
+  defaultReleaseBranch: 'main',
+  devDeps: ['projen-turborepo'],
+  name: 'projen-awscdk-construct',
+  projenrcTs: true,
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
